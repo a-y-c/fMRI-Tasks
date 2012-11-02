1d8561d244dff7b1eaba5cdb2067ac501ab72c17
a-y-c <andrew.cho.52@gmail.com>
2012-11-02 13:59:45 -0700

    update

---
+++ CardProb/Anaylsis.m
@@ -0,0 +1,31 @@
+function [ VAT ] = Anaylsis( Params, VAT )
+% VAT = ...
+% ParadigmJitter(BlockNumber, Params, ScreenHandels, VAT)
+%
+%******************************************************************
+%   INPUT:
+%       BlockNumber = Block Number
+%       VAT ->
+%               .StartTime      = Time Start
+%               .TimeStamp[]    = Time Log of Events
+%               .TSCode{}       = Name of Events
+%               .j              = Global Counter
+%               .Results{}      = Result Struct 
+%   
+%******************************************************************
+%
+%
+% Written by Cameron Rodriguez
+%   2012/02/08
+% Refractor by Andrew Cho
+%   2012/08/23
+%
+%******************************************************************
+%   DEPENDENCIES:
+%
+%   DEPENDENT BY:
+%       StaglingPTB.m
+%
+%******************************************************************
+
+

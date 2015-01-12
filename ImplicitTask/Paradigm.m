07e0831c5c499446ad5b75bcc7734c4e51d0235c
Crusader <andrew.cho.52@gmail.com>
2015-01-11 21:14:55 -0800

    implicit task

---
+++ ImplicitTask/Paradigm.m
@@ -0,0 +1,238 @@
+function TrialVariables = Paradigm(BlockNum, Params, ScreenHandels)
+% TrialVariables = ...
+% Paradigm(BlockNum, Params, ScreenHandels)
+%
+%******************************************************************
+%
+% Written by Cameron Rodriguez
+% Last Modified 2012/02/08
+% Modified by Andrew Cho
+%   FOR Alcohol Implicit TaskTask
+%   2014/12
+%
+%******************************************************************
+%
+%   DEPENDENCIES:
+%
+%   DEPENDENT BY:
+%       StaglingPTB
+%
+%******************************************************************
+
+%%%%%%%%%%%%%
+% CONSTANTS %
+%%%%%%%%%%%%%
+%% Activate Keyboard
+KbName('UnifyKeyNames');
+FlushEvents('keyDown');
+
+space=KbName('SPACE');
+esc=KbName('ESCAPE');
+right=KbName('RightArrow');
+left=KbName('LeftArrow');
+up=KbName('UpArrow');
+down=KbName('DownArrow');
+shift=KbName('RightShift');
+
+Keys.OKKey = KbName('p');
+Keys.KillKey = KbName('k');
+Keys.TRKey1 = KbName('t'); % TR signal key
+Keys.TRKey2 = KbName('5'); % TR signal key
+Keys.TRKB = KbName('5%');  % Keyboard TR
+
+Keys.KB1 = KbName('1!');   % Keyboard 1
+Keys.KB2 = KbName('2@');   % Keyboard 1
+Keys.KB3 = KbName('3#');   % Keyboard 1
+Keys.KB4 = KbName('4$');   % Keyboard 1
+
+Keys.BB1 = KbName('1');    % Button Box 1
+Keys.BB2 = KbName('2');    % Button Box 2
+Keys.BB3 = KbName('3');    % Button Box 3
+Keys.BB4 = KbName('4');    % Button Box 4
+
+% Keep KbCheck for looking for the TR signals
+olddisabledkeys = DisableKeysForKbCheck([KbName('T'),KbName('5')]); 
+    
+%% Unpack Stucts
+WES = ScreenHandels.WES;   % Window Handel Experimenter
+RES = ScreenHandels.RES;   % Window Rectangle Experimenter
+ifiE = ScreenHandels.ifiE; % interframe interval Experimenter
+WSS = ScreenHandels.WSS;   % Window Handel Subject
+RSS = ScreenHandels.RSS;   % Window Rectangle Subject
+ifiS = ScreenHandels.ifiS; % interframe interval Subject
+
+
+%%%%%%%%%
+% SETUP %
+%%%%%%%%%
+Task = Params.Task{BlockNum};
+Timing = Params.Timing;
+RED =  [ 255 0 0 ];
+
+% Presentation Order (Randomized)
+TotalWords = [ Task.LeftLeaningWord; Task.RightLeaningWord ];
+LeftRightDif = [ zeros(length(Task.LeftLeaningWord) ; ones(length(Task.RightLeaningWord) ];
+ix = randperm(length(TotalWords));
+
+LeftTextPos = Params.ScreenCenter / 2;
+RightTextPos = [ Params.ScreenCenter(1)*3/2 Params.ScreenCenter(2)/2 ];
+
+%% Preallocate Variable for Speed
+VAT.TimeStamps = nan(1, 100);
+VAT.ResponseChoice = nan(1, 100);
+VAT.ResponseTime  = nan(1, 100);
+VAT.CorrectTime  = nan(1, 100);
+VAT.BlockNum = nan(1, 100);
+VATCount = 1;
+
+
+%%%%%%%%%
+% INTRO %
+%%%%%%%%%
+Text = Task.Intro;
+DrawFormattedText(WSS, Text, 'center', 'center', 0 , 45);
+TimeStamp = Screen('Flip',WSS);
+
+VAT.TimeStamps(VATCount)        = TimeStamp;
+VAT.ResponseChoice(VATCount)    = 0; 
+VAT.ResponseTime(VATCount)     = 0;
+VAT.CorrectTime(VATCount)      = 0;
+VAT.BlockNum(VATCount)         = 0;
+VATCount = VATCount + 1;
+
+WaitSecs(Timing.Intro);
+
+
+%%%%%%%%%%%%%%
+% EXPERIMENT %
+%%%%%%%%%%%%%%
+for n = 1:length(TotalWords)
+
+    LeftText = Task.LeftWord;
+    RightText = Task.RightWord;
+
+    CenterText = TotalWords{ix(n)};
+    DrawFormattedText(WSS, CenterText, 'center', 'center', 0 , 45);
+    DrawFormattedText(WSS, LeftText, LeftTextPos(1), LeftTextPos(2), 0 , 45);
+    DrawFormattedText(WSS, RightText, RightTextPos(1), RightTextPos(2), 0 , 45);
+
+    % Flip Screen
+    TimeStamp = Screen('Flip',WSS);
+    
+    % Get Key Press 
+    [ RT, Response ] = GetKeyPressWithTimeOut(Keys, Timing.ResponseTime);
+
+    % If not the Correct Choice
+    WrongChoice = false;
+    if (LeftRightDif{ix(n)} == 1)
+        if (Response == 3) || (Response == 4)
+            WrongChoice = true;
+            Expected1 = 1;
+            Expected2 = 2;
+        end
+    else 
+        if (Response == 1) || (Response == 2)
+            WrongChoice = true;
+            Expected1 = 3;
+            Expected2 = 4;
+        end 
+    end
+
+    
+    baseTime = GetSecs;
+    maxTime = Timing.CorrectionTime;
+    while (WrongChoice)
+        DrawFormattedText(WSS, 'X', XT1, YT1, RED, 45);
+        CorrectTime = Screen('Flip', WSS, [], 1) 
+        
+        newRT = 0;
+        ResponseFix = 0;
+        [ newRT, ResponseFix ] = GetKeyPressWithTimeOut(Keys, maxTime);
+
+        if (ResponseFix == Expected1 || ResponseFix == Expected2)
+            WrongChoice = false;
+            
+        % If Time ran out 
+        maxTime = maxTime - (GetSecs - baseTime);
+        if ( maxTime > 0 )
+            WrongChoice = false;
+    end
+
+    % Store Record
+    VAT.TimeStamps(VATCount)       = TimeStamp;
+    VAT.ResponseChoice(VATCount)   = Response; 
+    VAT.ResponseTime(VATCount)     = RT;
+    VAT.CorrectTime(VATCount)      = CorrectTime;
+    VAT.BlockNum(VATCount)         = BlockNum;
+    VATCount = VATCount + 1;
+    
+% End Loop 
+end
+
+Screen('Close'); % Close the Open Textures
+
+%% Trim off the excess and pack in a stuct to pass out
+VAT.TimeStamps(isnan(VAT.TimeStamps)) = [];
+VAT.ResponseChoice(isnan(VAT.ResponseChoice)) = []; 
+VAT.ResponseTime(isnan(VAT.ResponseTime)) = [];
+VAT.CorrectTime(isnan(VAT.CorrectTime)) = [];
+VAT.BlockNum(isnan(VAT.BlockNum)) = [];
+ 
+TrialVariables = VAT;
+% End Function
+end
+
+%%%%%%%%%%%%%
+% FUNCTIONS %
+%%%%%%%%%%%%%
+%% Get Key Press with Timeout 
+function [ RT, Response ] = GetKeyPressWithTimeOut(Keys, maxTime)
+
+    baseTime = GetSecs;
+    gotgood = false;
+    
+    while ~gotgood
+        % while no key is pressed, wait a bit, then check again
+        if( GetSecs-baseTime > maxTime )
+            secs = baseTime + maxTime;
+            RT = maxTime;
+            Response = 'T'; % Capitalized
+            return; 
+        end 
+        WaitSecs(0.001);
+    
+        % got here? a key is down! retrieve the key and RT
+        keyIsDown = 0;
+        [keyIsDown, secs, keyCode] = KbCheck(-1);
+        if keyIsDown == 1
+           if (keyCode(Keys.TRKey1) | ... 
+               keyCode(Keys.TRKey2) | ... 
+               keyCode(Keys.TRKB))
+               keyIsDown = 0;
+            else
+                gotgood = true;
+            end 
+        end 
+    end
+
+    % do not pass control back until the key has been released
+    while KbCheck(-1)
+        WaitSecs(0.001);
+    end
+
+    % Format Output to be Read. Only Button Box
+    if keyCode(Keys.KB1) || keyCode(Keys.BB1)
+        Response = '1';
+    elseif keyCode(Keys.KB2) || keyCode(Keys.BB2)
+        Response = '2';
+    elseif keyCode(Keys.KB3) || keyCode(Keys.BB3)
+        Response = '3';
+    elseif keyCode(Keys.KB4) || keyCode(Keys.BB4)
+        Response = '4';
+    else
+        Response = '0';
+    end
+
+    RT = secs-baseTime;
+end
+
---
+++ ImplicitTask/Parameters.m
@@ -0,0 +1,197 @@
+function Params = Parameters(TestMode, TestSubject)
+%******************************************************************
+%
+% Description:
+%   [ Parameters ] loads parameters!
+%
+% Inputs:
+%   [TestMode] {Integer}
+%       1 = Test Trial
+%       0 = Real Trial
+%   [TestSubject] {String}
+%       Test Subject Name/ID for filename save.
+%
+%******************************************************************
+%
+% Written by Andrew Cho
+% Last Modified 2012/06/05
+%
+%******************************************************************
+%
+% Dependencies:
+%    NONE
+%
+% Used By:
+%   StaglinPTB
+%
+%******************************************************************
+
+%%%%%%%%%
+% Setup %
+%%%%%%%%%
+Params.ExperimentName = 'fMRI_AlcoholImpTask';
+Params.TestSubject = TestSubject;
+
+% Design Choice
+Params.Design = 'Block'; 
+if strcmpi(Params.Design, 'Block') == 1
+    Params.DesignType = 0;
+elseif strcmpi(Params.Design, 'Jitter') == 1
+    Params.DesignType = 1;
+end
+
+switch Params.DesignType
+    % Block
+    case 0
+        Params.BlockLenght = 10; % Block Durrantion in sec 
+        Params.BlockLenghtMax = 'N/A';
+        Params.BlockLenghtMin = 'N/A';
+        Params.ISILenght   = 5; % Inter-Stimulus interval in sec
+        Params.ISIMax = 'N/A';
+        Params.ISIMin = 'N/A';
+    % Jitter
+    case 1
+        Params.BlockLenght = 'N/A';
+        Params.BlockLenghtMax = 30; % Max Block Durrantion (sec)
+        Params.BlockLenghtMin =  5; % Min Block Durrantion (sec)
+        Params.ISILenght = 10; % Inter-Jitter Block interval in sec
+        Params.ISIMax = 5; % Max Inter-Stimulus interval (sec)
+        Params.ISIMin = 1; % Min Inter-Stimulus interval (sec)
+end
+
+
+%%%%%%%%%%%%
+% Hardware %
+%%%%%%%%%%%%
+Params.MonitorType = 'Projector'; % 'Goggles';
+%     if strcmpi(Params.Design, 'Projector') == 1
+%         MonitorType = 0;
+%     elseif strcmpi(Params.Design, 'Goggles') == 1
+%         MonitorType = 1;
+%     end
+Params.DrawFixationPt = 1; % 1 = On, 0 = Off
+Params.RTA = 0; %Real Time Analysis 1 = On, 0 = Off
+Params.UseOneMonitor = 1; % Only use 1 monitor otherwise 
+
+%% Constants
+Params.Constants.SSE = 0; % Location in deg of left screen center wrt the
+                   % test subject's fixation
+Params.Constants.ESE = 0; % Location in deg of Right screen center wrt the
+                   % Experimenter's fixation
+Params.Constants.XOffset = 0; % horizontal stimulus position 
+                       % (-)left, (+)right
+Params.Constants.YOffset = 0; % vertical stimulus position
+                       % (-)up, (+)down
+Params.Constants.Computer = Screen('Computer'); % Get The Computer ID
+Params.Constants.PTBVersion = Screen('Version'); % Get The PTB Version
+
+
+
+%%%%%%%%%%
+% OUTPUT %
+%%%%%%%%%%
+% Establish Output Files
+Params.Data_DIR = 'LOG/';
+Params.Backup_Data_DIR = ''; 
+Params.Filename  = [ Params.ExperimentName, '_', ...
+             datestr(now,'yyyymmdd_HHMMSS'), '_Subject_', TestSubject];
+%%%%%%%%%%
+% TIMING %
+%%%%%%%%%%
+Params.Timing.Intro = 10;
+Params.Timing.Intro = 2;
+Params.Timing.ResponseTime = 5;
+Params.Timing.ResponseTime = 2;
+Params.Timing.CorrectionTime = 5;
+Params.Timing.CorrectionTime = 2;
+
+%%%%%%%%%%%%%
+% TASK DATA %
+%%%%%%%%%%%%%
+Params.Task = {}
+
+% Task 1
+Params.Task{1}.Intro = 'If the center word belongs to a category on the left, press the Leftmost Button. If the center word belongs to a category on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key.'
+Params.Task{1}.LeftWord = 'Soda';
+Params.Task{1}.RightWord = 'Methamphetamine';
+Params.Task{1}.LeftLeaningWord = {  'Coke'; 
+                                    'Cassis'; 
+                                    'Sinas';
+                                    'Spa';
+                                    'Tonic';
+                                    'Juice' };
+
+Params.Task{1}.RightLeaningWord = { 'Crank';
+                                    'Crystal';
+                                    'Ice';
+                                    'Speed';
+                                    'Chalk';
+                                    'Tina' };
+                                    
+% Task 2
+Params.Task{2}.Intro = 'If the center word belongs to a category on the left, press the Leftmost Button. If the center word belongs to a category on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key.'
+Params.Task{2}.LeftWord = 'Positive';
+Params.Task{2}.RightWord = 'Negative';
+Params.Task{2}.LeftLeaningWord = {  'Sociable'; 
+                                    'Good'; 
+                                    'Pleasant';
+                                    'Nice';
+                                    'Enjoyable';
+                                    'Sympathetic' };
+
+Params.Task{2}.RightLeaningWord = { 'Antisocial';
+                                    'Bad';
+                                    'Unpleasant';
+                                    'Stupid';
+                                    'Obnoxious';
+                                    'Tedious' };
+% Task 3 
+Params.Task{3}.Intro = 'If the center word belongs to a categories on the left, press the Leftmost Button. If the center word belongs to a categories on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key'
+Params.Task{3}.LeftWord = 'Soda/Positive';
+Params.Task{3}.RightWord = 'Alcohol/Negative';
+Params.Task{3}.LeftLeaningWord = ...
+    [ Params.Task{1}.LeftLeaningWord; Params.Task{2}.LeftLeaningWord ];
+Params.Task{3}.RightLeaningWord = ...
+    [ Params.Task{1}.RightLeaningWord; Params.Task{2}.RightLeaningWord ];
+
+% Task 4 
+Params.Task{4}.Intro = 'Sort the same categories again.'
+Params.Task{4}.LeftWord = 'Soda/Positive';
+Params.Task{4}.RightWord = 'Alcohol/Negative';
+Params.Task{4}.LeftLeaningWord = ...
+    [ Params.Task{1}.LeftLeaningWord; Params.Task{2}.LeftLeaningWord ];
+Params.Task{4}.RightLeaningWord = ...
+    [ Params.Task{1}.RightLeaningWord; Params.Task{2}.RightLeaningWord ];
+
+% Task 5
+Params.Task{5}.Intro = 'If the center word belongs to a category on the left, press the Leftmost Button. If the center word belongs to a category on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key.'
+Params.Task{5}.LeftWord = 'Alcohol';
+Params.Task{5}.RightWord = 'Soda';
+Params.Task{5}.LeftLeaningWord = Params.Task{1}.RightLeaningWord;
+Params.Task{5}.RightLeaningWord = Params.Task{1}.LeftLeaningWord;
+
+% Task 6
+Params.Task{6}.Intro = 'If the center word belongs to a category on the left, press the Leftmost Button. If the center word belongs to a category on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key.'
+Params.Task{6}.LeftWord = 'Negative';
+Params.Task{6}.RightWord = 'Postive';
+Params.Task{6}.LeftLeaningWord = Params.Task{2}.RightLeaningWord;
+Params.Task{6}.RightLeaningWord = Params.Task{2}.LeftLeaningWord;
+
+% Task 7 
+Params.Task{7}.Intro = 'The Categoies have new configuration. \nIf the center word belongs to a categories on the left, press the Leftmost Button. If the center word belongs to a categories on the right, press the Rightmost Button. If you make an error, an X will appear - fix it by pressing the other key'
+Params.Task{7}.LeftWord = 'Alcohol/Positive';
+Params.Task{7}.RightWord = 'Soda/Negative';
+Params.Task{7}.LeftLeaningWord = ...
+    [ Params.Task{1}.RightLeaningWord; Params.Task{2}.LeftLeaningWord ];
+Params.Task{7}.RightLeaningWord = ...
+    [ Params.Task{1}.LeftLeaningWord; Params.Task{2}.RightLeaningWord ];
+
+% Task 8 
+Params.Task{8}.Intro = 'Sort the same categories again.'
+Params.Task{8}.LeftWord = 'Alcohol/Positive';
+Params.Task{8}.RightWord = 'Soda/Negative';
+Params.Task{8}.LeftLeaningWord = ...
+    [ Params.Task{1}.RightLeaningWord; Params.Task{2}.LeftLeaningWord ];
+Params.Task{8}.RightLeaningWord = ...
+    [ Params.Task{1}.LeftLeaningWord; Params.Task{2}.RightLeaningWord ];
+
